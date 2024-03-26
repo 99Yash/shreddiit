48b44d8f7fc3d598e41c17c2c183cccb3f2b338b
yash <yashgouravkar@gmail.com>
2024-03-26 17:09:32 +0530

    remove data

--- pscale_dump_shreddiit_main_20240316_115032/shreddiit.Subscription-schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `Subscription` (
-  `userId` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `subredditId` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`userId`,`subredditId`),
-  KEY `Subscription_subredditId_idx` (`subredditId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
--- pscale_dump_shreddiit_main_20240316_115032/shreddiit.Vote-schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `Vote` (
-  `postId` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `userId` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `voteType` enum('UP','DOWN') COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`postId`,`userId`),
-  KEY `Vote_postId_idx` (`postId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
